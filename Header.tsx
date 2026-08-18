f934f6ad5a48dc685a7bb7026025e59d29a4cf5c
isithaproperty <brian.klue@isitha.global>
2026-08-18 07:59:24 +0200

    Delete Header.tsx

--- Header.tsx
+++
@@ -1,2 +0,0 @@
-import Link from 'next/link';
-export function Header(){return <div className="top"><Link className="brand" href="/">ISITHA GLOBAL</Link><div className="nav"><Link href="/">My Onboarding</Link><Link href="/#training">My Training</Link><Link href="/admin">HR Admin</Link></div></div>}
