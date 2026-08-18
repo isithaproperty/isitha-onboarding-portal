7c629c72ed3d4ae56ef0cdf7b67f72f5f2ed064b
isithaproperty <brian.klue@isitha.global>
2026-08-18 08:04:25 +0200

    Delete route (1).ts

--- route (1).ts
+++
@@ -1,2 +0,0 @@
-import {NextResponse} from 'next/server';
-export async function POST(req:Request){const f=await req.formData();const slug=String(f.get('slug')||'');return NextResponse.redirect(new URL(`/training/${slug}?acknowledged=1`,req.url),303)}
